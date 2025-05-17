11a17d1e2592915bc92956770f14a02ea4cff560
mbairavi <53147880+mbairavi@users.noreply.github.com>
2025-05-17 12:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello from Flask running in ECS Fargate via DockerHub!"
+    return "✅ Hello from Flask running in ECS Fargate via DockerHub!!"
 
 @app.route('/health')
 def health():
